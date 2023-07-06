d9d2839e3dc8e2ffc7ed335e3f50cd93e4176302
overtime05 <overtime05@naver.com>
2023-07-06 16:56:18 +0900

    업로드

---
+++ src/Hello.java
@@ -0,0 +1,32 @@
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("\033[2J");
+
+		System.out.println("\033[33m");
+		System.out.println("\033[44m");
+
+		System.out.print("\033[10;20H");
+		System.out.println("Hello Java!");
+		System.out.println("\033[34m");
+		System.out.println("\033[41m");
+
+		System.out.print("\033[5;10H");
+		System.out.println("Hello JavaScript!");
+		
+		System.out.println("\033[33m");
+		System.out.println("\033[44m");
+
+		System.out.print("\033[20;5H");
+		System.out.println("Hello HTML!");
+
+		System.out.println("\033[34m");
+		System.out.println("\033[41m");
+
+		System.out.print("\033[15;40H");
+		System.out.println("Hello SQL!");
+
+		System.out.println("\033[0m");
+	}
+}
+
